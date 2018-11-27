ebcd81629c1cd3021432e8e3d916f6934a4daa76
Ovi Trif <ovi.artist@gmail.com>
2018-11-27 08:58:57 +0100

    Add unit test for navigating to parking detail view

--- app/src/test/java/com/sniped/ui/main/ParkingListPresenterTest.kt
+++ app/src/test/java/com/sniped/ui/main/ParkingListPresenterTest.kt
@@ -1,31 +1,52 @@
 package com.sniped.ui.main
 
+import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import com.sniped.api.ApiService
+import com.sniped.core.RxSchedulers
 import com.sniped.test.MockitoTest
+import com.sniped.ui.navigator.Navigator
 import com.sniped.ui.parking.IParkingList
 import com.sniped.ui.parking.ParkingListPresenter
-import com.sniped.ui.parking.detail.ParkingNavigator
+import com.sniped.ui.parking.detail.ParkingNavigatorFactory
+import com.sniped.ui.parking.domain.Parking
+import io.reactivex.schedulers.TestScheduler
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
 
 class ParkingListPresenterTest : MockitoTest() {
 
+    private val testScheduler = TestScheduler()
+
     @Mock private lateinit var view: IParkingList.View
-    @Mock private lateinit var parkingNavigator: ParkingNavigator
+    @Mock private lateinit var apiService: ApiService
+    @Mock private lateinit var rxSchedulers: RxSchedulers
+    @Mock private lateinit var parkingNavigatorFactory: ParkingNavigatorFactory
 
     private lateinit var presenter: ParkingListPresenter
 
     @Before
     fun setUp() {
+        whenever(rxSchedulers.io).thenReturn(testScheduler)
+        whenever(rxSchedulers.ui).thenReturn(testScheduler)
         presenter = ParkingListPresenter(
-                parkingNavigator)
+                view,
+                apiService,
+                rxSchedulers,
+                parkingNavigatorFactory)
     }
 
     @Test
-    fun whenClickingDetailButton_shouldNavigateToDetailView() {
-        presenter.onDetailButtonClick()
+    fun onItemClick_shouldNavigateToDetailView() {
+        val parkingNavigator = mock<Navigator>()
+        val item = Parking.NULL
+        whenever(parkingNavigatorFactory.create(item)).thenReturn(parkingNavigator)
 
+        presenter.onItemClick(item)
+
+        verify(parkingNavigatorFactory).create(item)
         verify(parkingNavigator).navigate()
     }
 }
